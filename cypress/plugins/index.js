d731950c7bdbd626ca21c42d6ac0370c70fe6b1b
Joost Baaij <joost@spacebabies.nl>
2021-03-29 14:18:12 +0200

    iterate on structured data

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,21 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const { exec } = require("child_process");
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  // generate the static html.
+  on("before:run", (details) => {
+    exec("npm run build", (error, stdout, stderr) => {
+      console.log(stdout);
+    });
+  });
+
+  // leave everything how we found it.
+  on("after:run", (details) => {
+    exec("npm run clean");
+  });
+};
