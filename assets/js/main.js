26c3beaf8394eff42b1f871e8b015abce042ace9
Joost Baaij <joost@spacebabies.nl>
2026-03-19 09:14:37 +0100

    Introduce new-style main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function onReady(callback) {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', callback, { once: true });
+  } else {
+    callback();
+  }
+}
+
+onReady(() => {
+
+});
