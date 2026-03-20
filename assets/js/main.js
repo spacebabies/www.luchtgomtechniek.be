52e27a143f143c44153c76dc0e6ddf56df90a574
Joost Baaij <joost@spacebabies.nl>
2026-03-20 17:54:42 +0100

    Drop the feather-icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 import { initLanguage } from "./modules/language.js";
 import { initHeader } from "./modules/header.js";
-import feather from 'feather-icons';
 import { initSwiper } from "./modules/swiper.js";
 import { initLeaflet } from "./modules/leaflet.js";
 
@@ -15,7 +14,6 @@ function onReady(callback) {
 onReady(() => {
   initLanguage();
   initHeader(document.querySelector("#site-head"));
-  feather.replace({ width: "1em", height: "1em" });
   initSwiper();
   initLeaflet();
 
